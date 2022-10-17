3c9b4430f0a66cdbdf990f43301018f435ef65da
roangelova <raangelova97@gmail.com>
2022-10-17 12:01:20 +0300

    add a 404 page and use hmml pages in routing

--- Node.js Online Shop/routes/admin.js
+++ Node.js Online Shop/routes/admin.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<h1>Add product</h1>')
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 router.post('/product', (req, res, next) => {
--- Node.js Online Shop/routes/shop.js
+++ Node.js Online Shop/routes/shop.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>Add product</h1>')
+    //requires an absolute path 
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    //WE DONT ADD SLASHES, THIS IS BUILT AUTOMATICALLY to be supported on Linux and Windows
 });
 
 module.exports = router; 
