e739458380fd553869aa44a9a7726cd0babf39f2
roangelova <raangelova97@gmail.com>
2022-10-17 11:50:09 +0300

    add admin filter + static html pages

--- Node.js Online Shop/app.js
+++ Node.js Online Shop/app.js
@@ -5,11 +5,14 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(adminRoutes);
+
+
+app.use('/admin',adminRoutes); //adds the admin part as a filter + commo starting segment
 app.use(shopRoutes);
 
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express</h1>')
+//catch all middleware
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(3000);
