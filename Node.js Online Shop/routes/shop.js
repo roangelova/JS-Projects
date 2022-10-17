31dc91a39df35363d9d6790082b49104a0717272
roangelova <raangelova97@gmail.com>
2022-10-17 16:18:10 +0300

    add ejs templating engne

--- Node.js Online Shop/routes/shop.js
+++ Node.js Online Shop/routes/shop.js
@@ -1,14 +1,22 @@
-const express = require('express');
 const path = require('path');
 
-const rootDir = require('../helpers/path.js');
+const express = require('express');
+
+const rootDir = require('../helpers/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    //requires an absolute path 
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
-    //WE DONT ADD SLASHES, THIS IS BUILT AUTOMATICALLY to be supported on Linux and Windows
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
-module.exports = router; 
+module.exports = router;
