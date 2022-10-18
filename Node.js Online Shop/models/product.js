0f2ea74b8332f8ecbac624049b543d2f7ea14a61
roangelova <raangelova97@gmail.com>
2022-10-18 15:49:49 +0300

    enhance app

--- Node.js Online Shop/models/product.js
+++ Node.js Online Shop/models/product.js
@@ -6,34 +6,34 @@ const p = path.join(
     'data',
     'products.json');
 
-const getProductsFromFile = (cb) => {
-
+const getProductsFromFile = cb => {
     fs.readFile(p, (err, fileContent) => {
         if (err) {
             cb([]);
         } else {
             cb(JSON.parse(fileContent));
         }
-    })
-}
-
-
+    });
+};
 
 module.exports = class Product {
-    constructor(title) {
+    constructor(title, imageUrl, description, price) {
         this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
 
     save() {
         getProductsFromFile(products => {
             products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err)
+            fs.writeFile(p, JSON.stringify(products), err => {
+                console.log(err);
             });
         });
-    };
+    }
 
     static fetchAll(cb) {
         getProductsFromFile(cb);
     }
-}
+};
