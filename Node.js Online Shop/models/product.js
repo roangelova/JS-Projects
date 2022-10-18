4545ed6f7c77fbf9f448dfe6dccbb1892277c983
roangelova <raangelova97@gmail.com>
2022-10-18 11:00:41 +0300

    add save products to fs

--- Node.js Online Shop/models/product.js
+++ Node.js Online Shop/models/product.js
@@ -1,15 +1,38 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
-module.exports = class Product {
+const getProductsFromFile = (cb) => {
+    const p = path.join(
+        __dirname,
+        'data',
+        'products.json');
+
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    })
+}
+
+
+
+module.exports =  class Product {
     constructor(title) {
         this.title = title;
     }
 
     save() {
-        products.push(this);
-    }
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err)
+            });
+        });
+    };
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
     }
 }
