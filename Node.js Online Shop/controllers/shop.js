79e20dcd16bd9c2b297f2188919bf683147ff33c
roangelova <raangelova97@gmail.com>
2022-10-19 14:04:02 +0300

    pre-populate edit form; remove add-product view and reuse add-product; clear unused code

--- Node.js Online Shop/controllers/shop.js
+++ Node.js Online Shop/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -41,8 +42,9 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const productId = req.body.productId;
-
-
+  Product.getById(productId, (product) => {
+    Cart.addProduct(productId, product.price);
+  })
   res.redirect('/cart');
 };
 
--- Node.js Online Shop/models/cart.js
+++ Node.js Online Shop/models/cart.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const p = path.join(
-  path.dirname(process.mainModule.filename),
+  __dirname,
   'data',
   'cart.json'
 );
