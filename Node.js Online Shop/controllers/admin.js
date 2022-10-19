b72d9bfe337093eb5683a7cd71fa8047dd460a63
roangelova <raangelova97@gmail.com>
2022-10-19 14:27:15 +0300

    implement edit product

--- Node.js Online Shop/controllers/admin.js
+++ Node.js Online Shop/controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
@@ -39,6 +39,19 @@ exports.getEditProduct = (req, res, next) => {
       product: product
     });
   })
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const productId = req.body.productId;
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+
+  const product = new Product(productId, title, imageUrl, description, price);
+  product.save();
+
+  res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
--- Node.js Online Shop/models/product.js
+++ Node.js Online Shop/models/product.js
@@ -22,7 +22,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -30,12 +31,21 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
+      if (this.id) {
+        const existingProdIndex = products.findIndex(p => p.id === this.id);
+        const updatedProducts = [...products];
+        updatedProducts[existingProdIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
     });
   }
 
