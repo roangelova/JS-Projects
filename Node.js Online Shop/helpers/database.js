d1399106c9993e24173ef6c625cbf71e9f5b9769
roangelova <raangelova97@gmail.com>
2022-10-20 16:39:44 +0300

    MODIFY DB CONNECTION

--- Node.js Online Shop/helpers/database.js
+++ Node.js Online Shop/helpers/database.js
@@ -1,14 +1,27 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-const uri = 'mongodb+srv://roan97:P@ssw1rd@shop-js.2e3xpfv.mongodb.net/?retryWrites=true&w=majority'
+const uri = 'mongodb+srv://ЯЕЪИЕШ:JKFGSKDF@shop-js.2e3xpfv.mongodb.net/?retryWrites=true&w=majority';
+//if the db does not exist, MongoDb will create it as soon as we start writing data to it
+
+let _db;
 
 const MongoConnect = callback => {
     const client = new MongoClient(uri, { useNewUrlParser: true });
     client.connect(err => {
         console.log("connected");
+        _db = client.db('js-shop'); // we are storing the access to the db 
         callback(client);
         client.close();
     });
 };
-module.exports = MongoConnect;
+
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No db found';
+}
+
+exports.getDb = getDb;
+exports.MongoConnect = MongoConnect;
