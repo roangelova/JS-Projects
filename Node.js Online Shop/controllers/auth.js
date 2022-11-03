b0b50e75a09035b73be7db828da548d5653f0698
roangelova <raangelova97@gmail.com>
2022-11-03 16:41:14 +0200

    add sign up

--- Node.js Online Shop/controllers/auth.js
+++ Node.js Online Shop/controllers/auth.js
@@ -18,6 +18,16 @@ exports.postLogin = (req, res, next) => {
         })
 };
 
+exports.getSignup = (req, res, next) => {
+    res.render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      isAuthenticated: false
+    });
+  };
+
+  exports.postSignup = (req, res, next) => {};
+
 exports.postLogout = (req, res, next) => {
     req.session.destroy(() => {
         //what to do once the session is destroyed
