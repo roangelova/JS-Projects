15eab9153684aacf9b1e7bcd4293b2570bda5219
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2023-03-24 12:21:53 +0200

    finish section 21: pagination;

--- Node.js Online Shop/controllers/shop.js
+++ Node.js Online Shop/controllers/shop.js
@@ -10,12 +10,30 @@ const ITEMS_PER_PAGE = 3;
 
 exports.getProducts = (req, res, next) => {
 
+  let totalItems;
+
+  const page = +req.query.page || 1;
+
   Product.find()
+    .countDocuments()
+    .then(numOfProducts => {
+      totalItems = numOfProducts;
+      return
+      Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render('shop/product-list', {
         prods: products,
-        pageTitle: 'All Products',
-        path: '/products'
+        pageTitle: 'Products',
+        path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       })
     }).catch(err => {
       const error = new Error(err);
@@ -39,26 +57,37 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  const numberOfProducts;
-  const page = req.query.page;
+  let totalItems;
 
-Product.find().count().then().catch()
+  const page = +req.query.page || 1;
 
   Product.find()
-  .skip((page -1) * ITEMS_PER_PAGE)
-  .limit(ITEMS_PER_PAGE)
-  .then(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/s'
+    .countDocuments()
+    .then(numOfProducts => {
+      totalItems = numOfProducts;
+      return
+      Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
     })
-  }).catch(err => {
-    const error = new Error(err);
-    error.httpStatusCode = 500;
+    .then(products => {
+      res.render('shop/index', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+      })
+    }).catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
 
-    return next(error);
-  })
+      return next(error);
+    })
 };
 
 exports.getCart = (req, res, next) => {
